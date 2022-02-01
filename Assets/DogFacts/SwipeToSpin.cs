279b8be4e734f322964172677f1118e573bde3fa
Alexander Lazaris <alex.lazaris617@gmail.com>
2022-02-01 22:07:03 +1100

    All unity assets

---
+++ Assets/DogFacts/SwipeToSpin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeToSpin : MonoBehaviour
+{
+    [SerializeField]
+    private float multiplierY=0.02f;
+    [SerializeField]
+    private float _speed = 100;
+    [SerializeField]
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.Rotate(Vector3.forward, _speed);
+            rb.AddForce(Vector3.up * 7);
+        }
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            transform.Rotate(Vector3.forward, _speed * -1);
+        }
+    }
+    
+}
